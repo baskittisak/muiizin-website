e2f86ae51c8278f5285b9d750cbd83d9d4579c16
baskittisak <basbubie.kittisak@gmail.com>
2022-04-10 00:43:08 +0700

    [EDIT] Footer Size Mobile

--- src/center_components/layout/Footer.js
+++ src/center_components/layout/Footer.js
@@ -1,5 +1,6 @@
 import { memo } from "react";
-import styled from "styled-components";
+import { useResponsive } from "../../utils/useResponsive";
+import styled, { css } from "styled-components";
 import { Row, Col, Space } from "antd";
 import Typography from "../../center_components/Typography";
 import Icon from "@ant-design/icons";
@@ -17,23 +18,54 @@ const FooterContainer = styled(Row)`
     align-items: center;
     justify-content: space-between;
   }
+
+  ${({ sm }) =>
+    sm &&
+    css`
+      height: 70px;
+      padding: 18px 0;
+
+      .ant-col {
+        flex-direction: column;
+      }
+    `};
+`;
+
+const SpaceSocial = styled(Space)`
+  ${({ sm }) =>
+    sm &&
+    css`
+      margin-bottom: 8px;
+    `};
 `;
 
 const IconSocial = styled(Icon)`
   cursor: pointer;
   font-size: 28px;
+
+  ${({ sm }) =>
+    sm &&
+    css`
+      font-size: 14px;
+    `};
 `;
 
 const Footer = () => {
+  const { sm } = useResponsive();
+
   return (
-    <FooterContainer>
+    <FooterContainer sm={sm}>
       <Col span={20} offset={2}>
-        <Space size={40}>
-          <IconSocial component={facebook_icon} />
-          <IconSocial component={line_icon} />
-          <IconSocial component={instagram_icon} />
-        </Space>
-        <Typography fontSize={24} lineHeight={26} color="#044700">
+        <SpaceSocial size={sm ? 15 : 40} sm={sm}>
+          <IconSocial component={facebook_icon} sm={sm} />
+          <IconSocial component={line_icon} sm={sm} />
+          <IconSocial component={instagram_icon} sm={sm} />
+        </SpaceSocial>
+        <Typography
+          fontSize={sm ? 12 : 24}
+          lineHeight={sm ? 13 : 26}
+          color="#044700"
+        >
           © 2022 Muiizin Co., Ltd
         </Typography>
       </Col>
