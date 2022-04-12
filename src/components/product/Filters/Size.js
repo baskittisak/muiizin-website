a19f1676be3e1eec58e7e7149a59f5c00bd5a76f
baskittisak <basbubie.kittisak@gmail.com>
2022-04-12 16:45:56 +0700

    [ADD] Size Filter

---
+++ src/components/product/Filters/Size.js
@@ -0,0 +1,77 @@
+import { memo, useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+import { Space } from "antd";
+import Checkbox from "../../../center_components/Checkbox";
+import FiltersCard from "./FiltersCard";
+
+const allSize = [
+  {
+    key: "s",
+    value: "S",
+    total: 15,
+  },
+  {
+    key: "m",
+    value: "M",
+    total: 10,
+  },
+  {
+    key: "l",
+    value: "L",
+    total: 8,
+  },
+  {
+    key: "xl",
+    value: "XL",
+    total: 5,
+  },
+];
+
+const SpaceContainer = styled(Space)`
+  width: 100%;
+`;
+
+const Size = () => {
+  const [sizeList, setSizeList] = useState([]);
+
+  useEffect(() => {
+    if (allSize) {
+      setSizeList(() =>
+        [...allSize].map((size) => ({
+          ...size,
+          checked: false,
+        }))
+      );
+    }
+  }, []);
+
+  const onChange = useCallback((id) => {
+    setSizeList((prevState) => {
+      const newsizeList = [...prevState];
+      const activeKey = newsizeList.find((size) => size.key === id);
+      if (activeKey) {
+        activeKey.checked = !activeKey.checked;
+      }
+      return newsizeList;
+    });
+  }, []);
+
+  return (
+    <FiltersCard title="size">
+      <SpaceContainer direction="vertical" size={10}>
+        {sizeList.map((size) => (
+          <Checkbox
+            key={size.key}
+            id={size.key}
+            value={size.value}
+            checked={size.checked}
+            total={size.total}
+            onChange={onChange}
+          />
+        ))}
+      </SpaceContainer>
+    </FiltersCard>
+  );
+};
+
+export default memo(Size);
